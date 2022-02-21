3ea33ab5b1c43d42ee8f7311e1b5c44ee817a05e
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-20 21:04:37 -0500

    Working

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -8,35 +8,27 @@ public class AddressBook {
     private String email;
     static Scanner input = new Scanner(System.in);
     static int placement;
-    public HashMap<Integer, PersonInfo> addressBook = new HashMap<>();
+    public HashMap<String, String> addressBook = new HashMap<>();
 
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 
     public void newContact(){
         System.out.println("Enter name");
         name = input.nextLine();
         System.out.println("Enter email");
         email = input.nextLine();
-        PersonInfo person = new PersonInfo(name, email);
+//        PersonInfo person = new PersonInfo(name, email);
         placement++;
-        addressBook.put(placement, person);
+        addressBook.put(name, email);
 
     }
 
 
 
     public void searchContact(){
-        PersonInfo person = new PersonInfo(name, email);
+//        PersonInfo person = new PersonInfo(name, email);
         System.out.println("Search for name");
-        int i = input.nextInt();
-        String getEmailAddress = String.valueOf(addressBook.get(i).getEmail());
+        String i = input.nextLine();
+        String getEmailAddress = String.valueOf(addressBook.get(i));
         System.out.println("There Email is: " + getEmailAddress); //Need to be able to search for person's email by looking up name in addressbook
     }
 
